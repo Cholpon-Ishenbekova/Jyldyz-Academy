6c14846e1afcde37c063cb03f399c25659b5e40e
Cholpon Ishenbekova <cholponymoonishka@gmail.com>
2020-09-28 18:42:59 +0600

    add 3 sections (our team, interviews, code)

--- js/slider__interviews__slick.js
+++ js/slider__interviews__slick.js
@@ -2,30 +2,34 @@ $('.interviews__list').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    dots: true,
+    prevArrow: '<span class="left-arrow"><img src="img/left.png" alt=""></span>',
+    nextArrow: '<span class="right-arrow"><img src="img/right1.png" alt=""></span>',
     responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
         }
-  
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      }
       ]
-  });
+    });
--- js/slider__team__slick.js
+++ js/slider__team__slick.js
@@ -1,9 +1,11 @@
 $('.team__list').slick({
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
+    prevArrow: '<span class="left-arrow"><img src="img/left.png" alt=""></span>',
+    nextArrow: '<span class="right-arrow"><img src="img/right1.png" alt=""></span>',
     responsive: [
       {
         breakpoint: 1024,
@@ -17,17 +19,18 @@ $('.team__list').slick({
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          dots: false
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          dots: false
         }
       }
-
-    ]
+      ]
   });
